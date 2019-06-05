6df32b9ba0be327ed328ee3ac9d347b6ed244a39
leeyebin <leeyebin007@gmail.com>
2019-06-05 22:34:00 +0900

    예제코드

---
+++ JSP/src/Hello2.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//주석달기
+@WebServlet("/hello2")
+public class Hello2 extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("name");
+		if (name == null) {
+			name = "자바 카페와 함께하는 Servlet/JSP 강의";
+		}
+		req.setAttribute("javacafe", name);
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/hello2.jsp");
+		dispatcher.forward(req, resp);
+	}
+}

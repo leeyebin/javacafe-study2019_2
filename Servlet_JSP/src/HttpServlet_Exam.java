c6bf6d5f98306e4c0a25f707f83266d3eced383d
leeyebin <leeyebin007@gmail.com>
2019-06-05 22:42:34 +0900

    web.xml처리

--- Servlet_JSP/src/HttpServlet_Exam.java
+++ Servlet_JSP/src/HttpServlet_Exam.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/HttpServlet")
 public class HttpServlet_Exam extends HttpServlet {
 
 	private int visitCount = 0;
--- Servlet_JSP/src/Servlet_Complex.java
+++ Servlet_JSP/src/Servlet_Complex.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/ComplexServlet")
 public class Servlet_Complex extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)

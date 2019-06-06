d69d41a8d1d4823acede4758772455db8c891778
leeyebin <leeyebin007@gmail.com>
2019-06-06 15:55:31 +0900

    JSP예제

--- JSP/src/Hello2.java
+++ JSP/src/Hello2.java
@@ -2,13 +2,10 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//주석달기
-@WebServlet("/hello2")
 public class Hello2 extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
